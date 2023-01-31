5873adb83ba3d5adfe4e1e71710653f20a3f81de
Yaido <35653@ma-web.nl>
2023-01-31 10:07:02 +0100

    Game toegevoegd aan de site

--- Website/js/canvas.js
+++ Website/js/canvas.js
@@ -1,7 +1,7 @@
 
   var canvas = document.getElementById('canvas');
   var g = canvas.getContext('2d'); 
-  var vierkantjes = [
+  var vierkant = [
     {x: 50, y: 50, w: 50, h: 50, visible: false},
     {x: 50, y: 100, w: 50, h: 50, visible: false},
     {x: 50, y: 150, w: 50, h: 50, visible: false},
@@ -10,37 +10,37 @@
     {x: 100, y: 150, w: 50, h: 50, visible: false},
     {x: 150, y: 50, w: 50, h: 50, visible: false},
     {x: 150, y: 100, w: 50, h: 50, visible: false},
-    {x: 150, y: 150, w: 50, h: 50, visible: false}
+    {x: 150, y: 150, w: 50, h: 50, visible: false},
   ];
-  function drawvierkantjes() {
+  function tekenvierkant() {
     g.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < vierkantjes.length; i++) {
-      var mole = vierkantjes[i];
+    for (var i = 0; i < vierkant.length; i++) {
+      var mole = vierkant[i];
       if (mole.visible) {
-        g.fillStyle = '#2596be';
+        g.fillStyle = "#2596be";
         g.fillRect(mole.x, mole.y, mole.w, mole.h);
-      }
-    }
-  }
-  function handleClick(event) {
-    var x = event.clientX - canvas.offsetLeft;
-    var y = event.clientY - canvas.offsetTop;
-
-    for (var i = 0; i < vierkantjes.length; i++) {
-      var mole = vierkantjes[i];
-      if (x > mole.x && x < mole.x + mole.w && y > mole.y && y < mole.y + mole.h && mole.visible) {
-        mole.visible = false;
-        drawvierkantjes();
       }
     }
   }
   canvas.addEventListener('click', handleClick);
   function showMole() {
-    var mole = vierkantjes[Math.floor(Math.random() * vierkantjes.length)];
+    var mole = vierkant[Math.floor(Math.random() * vierkant.length)];
     mole.visible = true;
     setTimeout(function() {
       mole.visible = false;
-      drawvierkantjes();
+      tekenvierkant();
     }, Math.random() * 2000 + 1000);
   }
   setInterval(showMole, 1000);
+  function handleClick(event) {
+    var x = event.clientX - canvas.offsetLeft;
+    var y = event.clientY - canvas.offsetTop;
+
+    for (var i = 0; i < vierkant.length; i++) {
+      var mole = vierkant[i];
+      if (x > mole.x && x < mole.x + mole.w && y > mole.y && y < mole.y + mole.h && mole.visible) {
+        mole.visible = false;
+        tekenvierkant();
+      }
+    }
+  }
